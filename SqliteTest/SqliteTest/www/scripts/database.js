3b0dc6c23f96653b09c4bc255d24e549f08a9c24
任跃兵 <cnryb@live.cn>
2017-11-03 17:00:35 +0800

    简单测试代码

---
+++ SqliteTest/SqliteTest/www/scripts/database.js
@@ -0,0 +1,114 @@
+﻿
+; (function () {
+    "use strict";
+
+    var _db,
+        _initSqls = [],
+        _clearSqls = [],
+        _initialized = false,
+        _upgradeSqls = [],
+        _schemaVersion = 11;
+
+
+    // 初始化数据库
+    function init() {
+        var dfr = $.Deferred(),
+            dbName = "demo.db";
+
+        if (_initialized) {
+            dfr.resolve();
+            return dfr.promise();
+        }
+        _initialized = true;
+        _db = window.sqlitePlugin.openDatabase({ name: dbName, location: 'default' });
+
+        return executeNonQuery("CREATE TABLE IF NOT EXISTS Demo (id INTEGER, name TEXT)");
+    }
+
+    // 清理数据库
+    function clearDb() {
+        return executeNonQuery("DELETE FROM Demo");
+        //return executeNonQuery("DROP TABLE IF EXISTS Demo");
+    }
+
+    // 执行非查询类的SQL语句，返回影响的行数
+    function executeNonQuery(sql, params) {
+        var dfr = $.Deferred();
+        if (!_db) {
+            dfr.reject();
+        } else {
+            _db.transaction(function (tx) {
+                tx.executeSql(sql, params,
+                    function (tx, res) {
+                        dfr.resolve(res.rowsAffected);
+                    }, function (tx, err) {
+                        dfr.reject(err.message);
+                    });
+            }, function (err) {
+                dfr.reject(err.message);
+            });
+        }
+        return dfr.promise();
+    }
+
+    // 执行多个非查询类的SQL语句
+    function executeNonQueries(sqlObjs) {
+        var dfr = $.Deferred();
+        if (!_db) {
+            dfr.reject();
+        } else {
+            _db.transaction(function (tx) {
+                $.each(sqlObjs, function (idx, sqlObj) {
+                    tx.executeSql(sqlObj.sql, sqlObj.params);
+                });
+            }, function (err) {
+                dfr.reject(err.message);
+            }, function () {
+                dfr.resolve();
+            });
+        }
+        return dfr.promise();
+    }
+
+    // 执行查询类的SQL语句，返回结果数组
+    function executeQuery(sql, params) {
+        var dfr = $.Deferred();
+        if (!_db) {
+            dfr.reject();
+        } else {
+            _db.transaction(function (tx) {
+                tx.executeSql(sql, params,
+                    function (tx, res) {
+                        // 转换为JavaScript数组，WebSQL和SQLite返回的结果格式略有不同
+                        var rows = [];
+                        if (typeof res.rows.item === "function") {
+                            for (var i = 0; i < res.rows.length; i++) {
+                                rows.push(res.rows.item(i));
+                            }
+                        } else {
+                            for (var i = 0; i < res.rows.length; i++) {
+                                rows.push(res.rows[i]);
+                            }
+                        }
+                        dfr.resolve(rows);
+                    }, function (tx, err) {
+                        dfr.reject(err.message);
+                    });
+            }, function (err) {
+                dfr.reject(err.message);
+            });
+        }
+        return dfr.promise();
+    }
+
+    window.mediPM = window.mediPM || {};
+
+    mediPM.database = {
+        init: init,
+        clearDb: clearDb,
+
+        executeNonQuery: executeNonQuery,
+        executeNonQueries: executeNonQueries,
+        executeQuery: executeQuery
+    };
+})();
--- SqliteTest/SqliteTest/www/scripts/index.js
+++ SqliteTest/SqliteTest/www/scripts/index.js
@@ -5,20 +5,50 @@
 (function () {
     "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
     function onDeviceReady() {
         // 处理 Cordova 暂停并恢复事件
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+
         // TODO: Cordova 已加载。在此处执行任何需要 Cordova 的初始化。
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        //var parentElement = document.getElementById('deviceready');
+        //var listeningElement = parentElement.querySelector('.listening');
+        //var receivedElement = parentElement.querySelector('.received');
+        //listeningElement.setAttribute('style', 'display:none;');
+        //receivedElement.setAttribute('style', 'display:block;');
+
+        $("#startTest").click(function (e) {
+            //var db = window.sqlitePlugin.openDatabase({ name: 'demo.db', location: 'default' });
+            var start = 0, end = 1000;
+            var startTime = new Date().getTime();
+
+            mediPM.database.init().then(function () {
+                return mediPM.database.clearDb();
+            }).then(function () {
+                //$.when.apply( )
+                startTime = new Date().getTime();
+
+                var qs = [];
+                for (var i = 0; i < end; i++) {
+                    qs.push(mediPM.database.executeNonQuery('INSERT INTO Demo VALUES (?,?)', [i, i]));
+                }
+                return $.when(...qs);
+            }).then(function () {
+                $("#msg").prepend(`<div> 写入${end - start}条数据，用时 ${new Date().getTime() - startTime} 毫秒</div>`);
+                startTime = new Date().getTime();
+                return mediPM.database.executeQuery("SELECT * FROM Demo WHERE id = ?", [end / 2]);
+            }).then(function (rows) {
+                console.log(rows);
+                $("#msg").prepend(`<div> 随机查询一条数据用时 ${new Date().getTime() - startTime} 毫秒</div>`);
+
+            }).fail(function (err) {
+                console.log(err);
+            });
+        });
     };
+
 
     function onPause() {
         // TODO: 此应用程序已挂起。在此处保存应用程序状态。
@@ -27,4 +57,11 @@
     function onResume() {
         // TODO: 此应用程序已重新激活。在此处还原应用程序状态。
     };
-} )();
+})();
+
+
+
+function rnd(n, m) {
+    var random = Math.floor(Math.random() * (m - n + 1) + n);
+    return random;
+}
