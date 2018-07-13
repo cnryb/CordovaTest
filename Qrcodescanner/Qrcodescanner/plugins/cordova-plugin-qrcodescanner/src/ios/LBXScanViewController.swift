12eb61edf364b349596e18c3c287a831334cd71a
任跃兵 <cnryb@live.cn>
2018-07-13 17:43:49 +0800

    简单测试

---
+++ Qrcodescanner/Qrcodescanner/plugins/cordova-plugin-qrcodescanner/src/ios/LBXScanViewController.swift
@@ -0,0 +1,260 @@
+//
+//  LBXScanViewController.swift
+//  swiftScan
+//
+//  Created by lbxia on 15/12/8.
+//  Copyright © 2015年 xialibing. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import AVFoundation
+
+
+open class LBXScanViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    open var scanObj: LBXScanWrapper?
+    
+    open var scanStyle: LBXScanViewStyle? = LBXScanViewStyle()
+    
+    open var qRScanView: LBXScanView?
+    
+    open var callBack:((String?, String?, Bool) -> Void)?
+    //public typealias callBackType = (String? , String? , Bool) -> Void
+    
+    
+    //启动区域识别功能
+    open var isOpenInterestRect = false
+    
+    //识别码的类型
+    var arrayCodeType:[String]?
+    
+    //是否需要识别后的当前图像
+    var isNeedCodeImage = false
+    
+    //禁止旋转，仅支持竖着的。其他的待研究实现方式
+    
+    open override var shouldAutorotate:Bool{
+        return false
+    }
+    
+    open override var supportedInterfaceOrientations:UIInterfaceOrientationMask{
+        return .portrait
+    }
+    
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        // [self.view addSubview:_qRScanView];
+        self.view.backgroundColor = UIColor.black
+        self.edgesForExtendedLayout = UIRectEdge(rawValue: 0)
+    }
+    
+    open func setNeedCodeImage(needCodeImg:Bool)
+    {
+        isNeedCodeImage = needCodeImg;
+    }
+    //设置框内识别
+    open func setOpenInterestRect(isOpen:Bool){
+        isOpenInterestRect = isOpen
+    }
+    
+    override open func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override open func viewDidAppear(_ animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        
+        drawScanView()
+        
+        perform(#selector(LBXScanViewController.startScan), with: nil, afterDelay: 0.3)
+        
+    }
+    
+    open func startScan()
+    {
+        if(!LBXPermissions .isGetCameraPermission())
+        {
+            showMsg(title: "提示", message: "没有相机权限，请到设置->隐私中开启本程序相机权限")
+            return;
+        }
+        
+        if (scanObj == nil)
+        {
+            var cropRect = CGRect.zero
+            if isOpenInterestRect
+            {
+                cropRect = LBXScanView.getScanRectWithPreView(preView: self.view, style:scanStyle! )
+            }
+            
+            //识别各种码，
+            //let arrayCode = LBXScanWrapper.defaultMetaDataObjectTypes()
+            
+            //指定识别几种码
+            if arrayCodeType == nil
+            {
+                arrayCodeType = [AVMetadataObjectTypeQRCode,AVMetadataObjectTypeEAN13Code,AVMetadataObjectTypeCode128Code]
+            }
+            
+            scanObj = LBXScanWrapper(videoPreView: self.view,objType:arrayCodeType!, isCaptureImg: isNeedCodeImage,cropRect:cropRect, success: { [weak self] (arrayResult) -> Void in
+                
+                if let strongSelf = self
+                {
+                    //停止扫描动画
+                    strongSelf.qRScanView?.stopScanAnimation()
+                    
+                    strongSelf.handleCodeResult(arrayResult: arrayResult)
+                }
+            })
+        }
+        
+        //结束相机等待提示
+        qRScanView?.deviceStopReadying()
+        
+        //开始扫描动画
+        qRScanView?.startScanAnimation()
+        
+        //相机运行
+        scanObj?.start()
+    }
+    
+    open func drawScanView()
+    {
+        if qRScanView == nil
+        {
+            qRScanView = LBXScanView(frame: self.view.frame,vstyle:scanStyle! )
+            self.view.addSubview(qRScanView!)
+        }
+        let btnCancel = UIButton()
+        btnCancel.setTitle("取消", for: UIControlState.normal)
+        
+        let yMax = self.view.frame.maxY - self.view.frame.minY
+        
+        let bottomItemsView = UIView(frame:CGRect( origin:CGPoint(x:0.0, y:yMax-60), size:CGSize(width:self.view.frame.size.width, height:60) ) )
+        
+        
+        bottomItemsView.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.6)
+        
+        self.view .addSubview(bottomItemsView)
+        
+        
+        let size = CGSize(width:65.0, height:87);
+        
+        
+        btnCancel.bounds = CGRect(origin:CGPoint(x:0,y:0), size:size)
+        btnCancel.center = CGPoint(x:bottomItemsView.frame.width/2, y:bottomItemsView.frame.height/2)
+        btnCancel.addTarget(self, action: #selector(LBXScanViewController.btnCancelAction), for: UIControlEvents.touchUpInside)
+        
+        bottomItemsView.addSubview(btnCancel)
+        
+        self.view .addSubview(bottomItemsView)
+        
+        qRScanView?.deviceStartReadying(readyStr: "相机启动中...")
+        
+    }
+    
+    func btnCancelAction() -> Void {
+        callBack!(nil,nil,true)
+    }
+    
+    
+    /**
+     处理扫码结果，如果是继承本控制器的，可以重写该方法,作出相应地处理
+     */
+    open func handleCodeResult(arrayResult:[LBXScanResult])
+    {
+        
+        let result:LBXScanResult = arrayResult[0]
+        
+        //showMsg(result.strBarCodeType, message: result.strScanned)
+        callBack?(result.strBarCodeType,result.strScanned,false)
+    }
+    
+    override open func viewWillDisappear(_ animated: Bool) {
+        
+        NSObject.cancelPreviousPerformRequests(withTarget: self)
+        
+        qRScanView?.stopScanAnimation()
+        
+        scanObj?.stop()
+    }
+    
+    open func openPhotoAlbum()
+    {
+        if(!LBXPermissions.isGetPhotoPermission())
+        {
+            showMsg(title: "提示", message: "没有相册权限，请到设置->隐私中开启本程序相册权限")
+        }
+        
+        let picker = UIImagePickerController()
+        
+        picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        picker.delegate = self;
+        
+        picker.allowsEditing = true
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    //MARK: -----相册选择图片识别二维码 （条形码没有找到系统方法）
+    @nonobjc open func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject])
+    {
+        picker.dismiss(animated: true, completion: nil)
+        
+        var image:UIImage? = info[UIImagePickerControllerEditedImage] as? UIImage
+        
+        if (image == nil )
+        {
+            image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        }
+        
+        if(image != nil)
+        {
+            let arrayResult = LBXScanWrapper.recognizeQRImage(image: image!)
+            if arrayResult.count > 0
+            {
+                handleCodeResult(arrayResult: arrayResult)
+                return
+            }
+        }
+        
+        showMsg(title: "", message: "识别失败")
+    }
+    
+    func showMsg(title:String?,message:String?)
+    {
+        if LBXScanWrapper.isSysIos8Later()
+        {
+            
+            //if #available(iOS 8.0, *)
+            
+            let alertController = UIAlertController(title: title, message:message, preferredStyle: UIAlertControllerStyle.alert)
+            let alertAction = UIAlertAction(title:  "知道了", style: UIAlertActionStyle.default) { [weak self] (alertAction) in
+                
+                if let strongSelf = self
+                {
+                    strongSelf.startScan()
+                }
+            }
+            
+            alertController.addAction(alertAction)
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+    deinit
+    {
+        print("LBXScanViewController deinit")
+    }
+    
+}
+
+
+
+
+
---
+++ Qrcodescanner/Qrcodescanner/plugins/cordova-plugin-qrcodescanner/src/ios/QRCodeScannerPlugin.swift
@@ -0,0 +1,87 @@
+﻿import UIKit
+import Foundation
+import AVFoundation
+
+@available(iOS 8.0, *)
+@objc(HWPQRCodeScanner) class QRCodeScannerPlugin : CDVPlugin {
+    var scanCommand:CDVInvokedUrlCommand?
+    
+    @objc(scan:)
+    func scan(command: CDVInvokedUrlCommand) {
+        //var message = command.arguments[0] as String
+        scanCommand=command;
+        
+        if(!LBXPermissions.isGetCameraPermission()){
+            
+            let cv=UIAlertController(title: "提示", message: "未获得授权使用摄像头，请在设置中打开", preferredStyle: .alert);
+            let okAction=UIAlertAction(title: "设置", style: .default, handler: {
+                action in
+                UIApplication.shared.openURL(URL(string:UIApplicationOpenSettingsURLString)!)
+            });
+            let cancelAction=UIAlertAction(title: "取消", style: .cancel, handler: nil);
+            cv.addAction(okAction);
+            cv.addAction(cancelAction);
+            self.viewController?.present(cv, animated: true,completion: nil);
+            
+            
+            
+            let result=["format":nil ,"text":nil,"cancelled":true] as [String : Any]
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK , messageAs:result)
+            
+            
+            self.commandDelegate.send(pluginResult, callbackId:self.scanCommand!.callbackId);
+            return;
+            
+        }
+        
+        //扫码框占屏幕的百分比
+        let frameWith=0.6;
+        var screenWith=UIScreen.main.bounds.width;
+        if(UIDevice.current.orientation==UIDeviceOrientation.landscapeLeft||UIDevice.current.orientation==UIDeviceOrientation.landscapeRight)
+        {
+            screenWith=UIScreen.main.bounds.height;
+        }
+        
+        let offset =  Double(screenWith)*(1-frameWith)/2;
+        
+        
+        //设置扫码区域参数
+        var style = LBXScanViewStyle()
+        //style.centerUpOffset = 44;
+        style.photoframeAngleStyle = LBXScanViewPhotoframeAngleStyle.On;
+        style.photoframeLineW = 6;
+        style.photoframeAngleW = 24;
+        style.photoframeAngleH = 24;
+        style.isNeedShowRetangle = true;
+        
+        style.anmiationStyle = LBXScanViewAnimationStyle.NetGrid;
+        
+        
+        //矩形框离左边缘及右边缘的距离
+        style.xScanRetangleOffset = CGFloat(offset);
+        
+        //使用的支付宝里面网格图片
+        style.animationImage = UIImage(named: "CDVQRCodeScanner.bundle/qrcode_scan_part_net")
+        
+        let vc = LBXScanViewController();
+        vc.scanStyle = style
+        
+        
+        vc.isOpenInterestRect = true
+        vc.callBack=callBack
+        
+        self.viewController?.present(vc, animated: true,completion: nil)
+        
+        
+    }
+    
+    func callBack(codeType:String?,codeResult:String?,isCancelled:Bool) -> Void {
+        self.viewController?.dismiss(animated:false, completion: nil)
+        
+        
+        let result=["format":codeType ,"text":codeResult,"cancelled":isCancelled] as [String : Any]
+        
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: result)
+        commandDelegate.send(pluginResult, callbackId:scanCommand!.callbackId)
+    }
+}
